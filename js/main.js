517a8c05657684b49a00d465631f75780b6276d3
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-08-20 19:21:46 +0300

    Реализовал отправку данных на почту и всплывающее окно; отправлен запрос на ssl; разместил на домене для проверки

--- js/main.js
+++ js/main.js
@@ -94,4 +94,73 @@ $(document).ready(function () {
     },
     keyboard: true,
   });
+
+  // Модальные окна
+  var modalButton = $("[data-toggle=modal]");
+  var closemodalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closemodalButton.on("click", closeModal);
+  var closemodalClick = $(".modal__overlay");
+  closemodalClick.on("click", closeModal);
+  $("[data-toggle=modal]").click(function () {
+    $("body").addClass("lock");
+    return false;
+  });
+  $(".modal__close").click(function () {
+    $("body").toggleClass("lock");
+    return false;
+  });
+  $(".modal__overlay").click(function () {
+    $("body").toggleClass("lock");
+    return false;
+  });
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+  $(".modal")
+    .on("shown", function () {
+      $("body").css("overflow", "hidden");
+    })
+    .on("hidden", function () {
+      $("body").css("overflow", "auto");
+    });
+
+  // Обработка форм(маска и валидация)
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Введите имя",
+          minlength: "Не менее 2 букв",
+        },
+        email: {
+          required: "Введите email",
+          email: "Формат: name@domain.com",
+        },
+        emailstwo: {
+          required: "Введите email",
+          email: "Формат: name@domain.com",
+        },
+        phone: {
+          required: "Введите номер телефона",
+          minlength: "Не менее 10 символов",
+        },
+      },
+    });
+  });
+
+  $(".phone").mask("+7 (999) 999-99-99");
 });
